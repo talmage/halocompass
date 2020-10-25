b9cc711ddfb3d131c6c1bddf48e46de30956d33b
David Talmage <talmage@acm.org>
2020-10-25 15:50:02 -0400

    Added some comments

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// Return the heading as a string.  E.g. A heading between 22.5 and 67.5 degrees is "NE".
 function degreesToDirection (degrees: number) {
     if (degrees <= 22.5) {
         direction = "N"
@@ -20,6 +21,7 @@ function degreesToDirection (degrees: number) {
     }
     return direction
 }
+// Map compass degrees into the corresponding LED on the HaloHD.  The range of valid inputs is 0-359.
 function degreesToLED (degrees: number) {
     return Math.trunc(degrees / 6)
 }
@@ -32,6 +34,7 @@ let haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
 haloDisplay.clear()
 haloDisplay.setBrightness(127)
 haloDisplay.show()
+// Every 100ms: update the heading in the 5x5 LED array and the North indicator in the HaloHD.
 basic.forever(function () {
     heading = input.compassHeading()
     currentNorthLED = degreesToLED(360 - heading)
